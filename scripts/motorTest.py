84d941fe0fe3c05d00f76425a30dbe0bd0f51c7a
Naiwen Xiao <naiwen.xiao@spc.ox.ac.uk>
2026-07-16 18:06:45 +0100

    start the first part of the motor test

---
+++ scripts/motorTest.py
@@ -0,0 +1,9 @@
+from src.motor.motor import *
+
+# graspMotor = Grasp_Motor()
+upDownMotor = Up_Down_Motor()
+leftRightMotor = Left_Right_Motor()
+
+# graspMotor.reset()
+upDownMotor.reset()
+leftRightMotor.reset()
---
+++ src/motor/motor.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python3
+from dynamixel_sdk import *
+import time
+
+pi = "/dev/ttyUSB0"
+window_device = "COM7"
+PORTHANDLER = PortHandler(pi)
+PACKETHANDLER = PacketHandler(2.0)
+
+PORTHANDLER.openPort()
+PORTHANDLER.setBaudRate(57600)
+
+# Mode the motor use
+OPERATING_MODE_ADDRESS = 11
+CURRENT_CONTROL_MODE = 0
+VELOCITY_CONTROL_MODE = 1
+POSITION_CONTROL_MODE = 3
+
+# things the motor need to changes
+TORQUE_ENABLE_ADDRESS = 64
+GOAL_CURRENT_ADDRESS = 102
+GOAL_VELOCITY_ADDRESS = 104
+GOAL_POSITION_ADDRESS = 116
+PRESENT_POSITION_ADDRESS = 132
+PRESENT_VELOCITY_ADDRESS = 128
+
+
+# motor idx 
+LEFT_RIGHT_IDX = 1
+UP_DOWN_IDX = 2
+GRASP_IDX = 3
+
+
+class Left_Right_Motor():
+    def __init__(self, k_torque = 0, k_acc = 0, k_vel = 0):
+        self.k_torque = k_torque
+        self.k_acc = k_acc
+        self.k_vel = k_vel
+        self.startPosition = 2048
+        self.minPosition_minus_70 = 1252
+        self.maxPosition_plus_70 = 2844
+        self.currentLimit = 910
+        self.id = LEFT_RIGHT_IDX
+
+    def reset(self):
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 0)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, OPERATING_MODE_ADDRESS, POSITION_CONTROL_MODE)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 1)
+        PACKETHANDLER.write4ByteTxRx(PORTHANDLER, self.id, GOAL_POSITION_ADDRESS, self.startPosition)
+        time.sleep(10)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 0)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, OPERATING_MODE_ADDRESS, CURRENT_CONTROL_MODE)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 1)
+
+    def detectPositionVelocity(self):
+        pos, _, _ = PACKETHANDLER.read4ByteTxRx(PORTHANDLER, self.id, PRESENT_POSITION_ADDRESS)
+        vec, _, _ = PACKETHANDLER.read4ByteTxRx(PORTHANDLER, self.id, PRESENT_VELOCITY_ADDRESS)
+        if vec > 0x7FFFFFFF:
+            vec -= 0x100000000
+        return pos, vec
+
+    def runTorque(self, current):
+        print("current limit: " + str(self.currentLimit))
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, current & 0xFFFF)
+        time.sleep(0.2)
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, 0)
+
+    def inverseTorque(self, wheel_current, acc_cmd, acc_max, vel_cmd, vel_max):
+        current = int(self.k_torque * wheel_current + self.k_acc * max(acc_cmd - acc_max, 0) + self.k_vel * max(vel_cmd - vel_max, 0))
+        self.runTorque(current)
+
+
+class Up_Down_Motor():
+    def __init__(self, k_torque = 0, k_acc = 0, k_vel = 0):
+        self.k_torque = k_torque
+        self.k_acc = k_acc
+        self.k_vel = k_vel
+        self.startPosition_30 = 2389
+        self.minPosition_0 = 2048
+        self.maxPosition_60 = 2731
+        self.currentLimit = 910
+        self.id = UP_DOWN_IDX
+
+    def reset(self):
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 0)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, OPERATING_MODE_ADDRESS, POSITION_CONTROL_MODE)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 1)
+        PACKETHANDLER.write4ByteTxRx(PORTHANDLER, self.id, GOAL_POSITION_ADDRESS, self.startPosition_30)
+        time.sleep(10)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 0)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, OPERATING_MODE_ADDRESS, CURRENT_CONTROL_MODE)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 1)
+
+    def detectPositionVelocity(self):
+        pos, _, _ = PACKETHANDLER.read4ByteTxRx(PORTHANDLER, self.id, PRESENT_POSITION_ADDRESS)
+        vec, _, _ = PACKETHANDLER.read4ByteTxRx(PORTHANDLER, self.id, PRESENT_VELOCITY_ADDRESS)
+        if vec > 0x7FFFFFFF:
+            vec -= 0x100000000
+        return pos, vec
+
+    def runTorque(self, current):
+        print("current limit: " + str(self.currentLimit))
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, current & 0xFFFF)
+        time.sleep(0.2)
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, 0)
+
+    def inverseTorque(self, wheel_current, acc_cmd, acc_max, vel_cmd, vel_max):
+        current = int(self.k_torque * wheel_current + self.k_acc * max(acc_cmd - acc_max, 0) + self.k_vel * max(vel_cmd - vel_max, 0))
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, current & 0xFFFF)
+        time.sleep(0.5)
+        PACKETHANDLER.write2ByteTxRx(PORTHANDLER, self.id, GOAL_CURRENT_ADDRESS, 0)
+
+
+class Grasp_Motor():
+    def __init__(self, k_slip):
+        self.k_slip = k_slip
+        self.max_speed_unit = 445
+        self.max_speed_rpm = 101.85
+        self.id = GRASP_IDX
+
+    def reset(self):
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 0)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, OPERATING_MODE_ADDRESS, VELOCITY_CONTROL_MODE)
+        PACKETHANDLER.write1ByteTxRx(PORTHANDLER, self.id, TORQUE_ENABLE_ADDRESS, 1)
+
+    def runVel(self, vel):
+        print("max speed unit: " + str(self.max_speed_unit))
+        PACKETHANDLER.write4ByteTxRx(PORTHANDLER, self.id, GOAL_VELOCITY_ADDRESS, vel & 0xFFFFFFFF)
+       
+    def speed(self, rotation_speed, slip_ratio):
+        if slip_ratio < 0:
+            slip_ratio = 0
+ 
+        speed = slip_ratio * self.k_slip
+        if rotation_speed >= 0:
+            vel = speed
+            self.runVel(vel)
+        else:
+            vel = speed * -1
+            self.runVel(vel)
+
+
+
+
+
+
